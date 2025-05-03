ab9d6cb175260cf57e8a1d40b90389819ec99772
owendev93 <owenpuertasauto@gmail.com>
2025-05-03 20:00:34 -0300

    Update terraform file

---
+++ packer-aws-ami-terraform/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+    type = string
+}
+
+variable "ami_id" {
+    type = string
+}
+
+variable "key_name" {
+    type = string
+}
+
+variable "security_group_id" {
+    type = string
+}
