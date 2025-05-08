19b92cc1b4d88c65ce23f036a4eaf4598316ceb5
Owen Puerta Sauto <owenpuertasauto@gmail.com>
2025-05-08 08:59:07 -0300

    Update file

--- packer-terraform-aws-azure/packer/provisioners/app.js
+++ packer-terraform-aws-azure/packer/provisioners/app.js
@@ -1,5 +1,10 @@
-// This app listens on port 3000 and responds with "Hello World!" for requests to the root URL (/) or route. 
-// (For every other path, it will respond with a 404 Not Found.)
+/*Esta aplicación Node.js está configurada para escuchar en el puerto 3000 y responde con el mensaje definido en el código cuando se 
+accede a la URL raíz (/). Este comportamiento básico permite comprobar rápidamente que el servidor está funcionando correctamente.
+Para cualquier otra ruta diferente de /, la aplicación devuelve un código de error "404 Not Found", indicando que la ruta solicitada
+no está disponible. Esta estructura sencilla es ideal como punto de partida para desarrollar aplicaciones web más complejas, ya que
+establece una ruta principal de respuesta y una gestión básica de errores para rutas no definidas.*/
+
+
 const http = require('http');
 
 const hostname = '0.0.0.0';
